ed00ed66073d6e08740fc87516ca8a3ce1e26501
JohnProgrammer71 <46646751+JohnProgrammer71@users.noreply.github.com>
2021-04-19 15:17:16 +0200

    Reformatted admin command 'editbonuses': argument 'name' moved before literal 'set|reset'

--- src/main/kotlin/io/github/cadrizor_team/cadrizor_bot/admin/EditBonusesCommand.kt
+++ src/main/kotlin/io/github/cadrizor_team/cadrizor_bot/admin/EditBonusesCommand.kt
@@ -6,8 +6,8 @@ import com.mojang.brigadier.arguments.StringArgumentType.getString
 import com.mojang.brigadier.arguments.StringArgumentType.word
 import com.mojang.brigadier.builder.LiteralArgumentBuilder.literal
 import com.mojang.brigadier.builder.RequiredArgumentBuilder.argument
-import io.github.cadrizor_team.cadrizor_bot.lang.GMREvent
 import io.github.cadrizor_team.cadrizor_bot.Main
+import io.github.cadrizor_team.cadrizor_bot.lang.GMREvent
 import io.github.cadrizor_team.cadrizor_bot.lang.isOwner
 import io.github.cadrizor_team.cadrizor_bot.storage.Bonuses
 import io.github.cadrizor_team.cadrizor_bot.storage.Bonuses.NO_BONUSES
@@ -18,58 +18,30 @@ import java.time.Instant
 object EditBonusesCommand {
 	@JvmStatic
 	fun editBonuses() = literal<GMREvent>("editbonuses")
-			.then(setValue())
-			.then(resetValue())
-			.requires(GMREvent::isOwner)!!
+			.requires(GMREvent::isOwner)
+			.then(argument<GMREvent, String>("name", word())
+					.then(setValue())
+					.then(resetValue()))!!
 
 	private fun setValue() = literal<GMREvent>("set")
-			.then(argument<GMREvent, String>("name", word())
-					.then(argument<GMREvent, Float>("value", floatArg(NO_BONUSES))
-							.executes {
-								BonusesStorage.retrieveBonuses()
-								val bonus = getString(it, "name")
-								val value = getFloat(it, "value")
-								when (bonus) {
-									"woodcutter" -> Bonuses.bonusWoodcutter = value
-									"miner" -> Bonuses.bonusMiner = value
-									"xp" -> Bonuses.bonusXP = value
-									else -> {}
-								}
-								BonusesStorage.storeBonuses()
-								Main.shardManager.shards.forEach { jda ->
-									jda.guilds.forEach { guild ->
-										CadrizEmbedBuilder.create()
-												.setTitle("Cadrizor Bot - Bonus Modification")
-												.addField("Bonus", bonus, true)
-												.addField("Value", "`${value * 100}%`", true)
-												.setFooter("Issued by ${it.source.member!!}")
-												.setTimestamp(Instant.now())
-												.send(guild)
-									}
-								}
-								1
-							}))!!
-
-	private fun resetValue() = literal<GMREvent>("reset")
-			.then(argument<GMREvent, String>("name", word())
+			.then(argument<GMREvent, Float>("value", floatArg(NO_BONUSES))
 					.executes {
 						BonusesStorage.retrieveBonuses()
 						val bonus = getString(it, "name")
+						val value = getFloat(it, "value")
 						when (bonus) {
-							"woodcutter" -> Bonuses.bonusWoodcutter = NO_BONUSES
-							"miner" -> Bonuses.bonusMiner = NO_BONUSES
-							"xp" -> Bonuses.bonusXP = NO_BONUSES
-							else -> {
-							}
+							"woodcutter" -> Bonuses.bonusWoodcutter = value
+							"miner" -> Bonuses.bonusMiner = value
+							"xp" -> Bonuses.bonusXP = value
+							else -> {}
 						}
 						BonusesStorage.storeBonuses()
 						Main.shardManager.shards.forEach { jda ->
 							jda.guilds.forEach { guild ->
 								CadrizEmbedBuilder.create()
 										.setTitle("Cadrizor Bot - Bonus Modification")
-										.appendDescription("Bonus $bonus was reset to its initial state")
 										.addField("Bonus", bonus, true)
-										.addField("Value", "`${NO_BONUSES * 100}%`", true)
+										.addField("Value", "`${value * 100}%`", true)
 										.setFooter("Issued by ${it.source.member!!}")
 										.setTimestamp(Instant.now())
 										.send(guild)
@@ -77,4 +49,31 @@ object EditBonusesCommand {
 						}
 						1
 					})!!
+
+	private fun resetValue() = literal<GMREvent>("reset")
+			.executes {
+				BonusesStorage.retrieveBonuses()
+				val bonus = getString(it, "name")
+				when (bonus) {
+					"woodcutter" -> Bonuses.bonusWoodcutter = NO_BONUSES
+					"miner" -> Bonuses.bonusMiner = NO_BONUSES
+					"xp" -> Bonuses.bonusXP = NO_BONUSES
+					else -> {
+					}
+				}
+				BonusesStorage.storeBonuses()
+				Main.shardManager.shards.forEach { jda ->
+					jda.guilds.forEach { guild ->
+						CadrizEmbedBuilder.create()
+								.setTitle("Cadrizor Bot - Bonus Modification")
+								.appendDescription("Bonus $bonus was reset to its initial state")
+								.addField("Bonus", bonus, true)
+								.addField("Value", "`${NO_BONUSES * 100}%`", true)
+								.setFooter("Issued by ${it.source.member!!}")
+								.setTimestamp(Instant.now())
+								.send(guild)
+					}
+				}
+				1
+			}!!
 }
