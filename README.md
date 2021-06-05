817810546ae07b01f432b829d5d0fa56d49f020b
JohnProgrammer71 <46646751+JohnProgrammer71@users.noreply.github.com>
2021-06-05 17:14:03 +0200

    Version 0.5.3 : - Using Jetbrains Space :   * Main Git repository is https://git.jetbrains.space/john71/cadrizor-bot/CadrizorBot.git   * Created CODEOWNERS and README.md - Changed messages for non-commands messages in commands channels

---
+++ README.md
@@ -0,0 +1,4 @@
+Cadrizor Bot
+============
+
+See documentation website [here](https://cadrizorteam.cf/bot)
--- src/main/kotlin/io/github/cadrizor_team/cadrizor_bot/handling/events/GuildMessageReceived.kt
+++ src/main/kotlin/io/github/cadrizor_team/cadrizor_bot/handling/events/GuildMessageReceived.kt
@@ -11,6 +11,7 @@ object GuildMessageReceived : EventHandler<GMREvent> {
 		val content = event.message.contentRaw
 		if (!Main.PROCESS_COMMANDS || event.author.isBot || event.isWebhookMessage) return
 		if ("${event.guild.id}|${event.author.id}" in Main.IGNORED) return
+
 		if (content.startsWith(Main.prefixes.admin)) {
 			if (event.channel.name != "commands-cadrizor${if (Main.dev) "-dev" else ""}") {
 				Utils.sendVolatileMessage(event, ":x: You're not in a commands channel (`commands-cadrizor${if (Main.dev) "-dev" else ""}`)")
@@ -39,17 +40,18 @@ object GuildMessageReceived : EventHandler<GMREvent> {
 			}
 			return
 		}
+
 		if (!content.startsWith(Main.prefixes.admin) && !content.startsWith(Main.prefixes.user)) {
 			if (event.channel.name != "commands-cadrizor${if (Main.dev) "-dev" else ""}") return
 			val message = event.message
 			val name = event.member!!.effectiveName
 			val id = event.author.id
-			val textChannel =
-					if (message.guild.idLong == event.guild.idLong) "<#${message.textChannel.id}>" else "*${message.textChannel.name}*"
+			val textChannel = if (message.guild.idLong == event.guild.idLong)
+				"<#${message.textChannel.id}>" else "*${message.textChannel.name}*"
 			var sendMessage = "@$name (`$id`) said :\n> ${message.contentDisplay}"
 			sendMessage += "\nin the channel $textChannel guild *`${message.guild.name}`*"
-			sendMessage += "\nYou can use `ca%resetiag $id`, `ca%reset \"<@!$id>\"` or `ca%ignore add \"<@!$id>\"`"
-			sendMessage += "\n*(NB: this works only in-\"game\" & for the Cadrizor @Owners)*"
+			sendMessage += "\nYou can use `ca%reset member this|all \"<@!$id>\"` or `ca%ignore add \"<@!$id>\"`"
+			sendMessage += "\n*(NB: this only works in commands channels and for the Cadrizor @Owners)*"
 			Main.TC_Cadrizor_admins.sendMessage(sendMessage).queue()
 			message.delete().queue()
 		}
